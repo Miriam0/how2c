b360a1a6ec116eae760689abe32fb90a270d7719
Krunker <Krunker@LAPTOP-EFFABB3H>
2020-09-15 08:41:05 +0300

    15й урок

--- ConsoleApp666/ConsoleApp666/Source.cpp
+++ ConsoleApp666/ConsoleApp666/Source.cpp
@@ -1,7 +1,19 @@
-#include <windows.h>
-#include <stdio.h>
+#include <iostream>
 
-int main() {
-	printf("Hello World!");
-	return 0;
+void doMath(int a)
+{
+    int b = 5;
+    std::cout << "doMath: a = " << a << " and b = " << b << std::endl;
+    a = 4;
+    std::cout << "doMath: a = " << a << " and b = " << b << std::endl;
+}
+
+int main()
+{
+    int a = 6;
+    int b = 7;
+    std::cout << "main: a = " << a << " and b = " << b << std::endl;
+    doMath(a);
+    std::cout << "main: a = " << a << " and b = " << b << std::endl;
+    return 0;
 }
